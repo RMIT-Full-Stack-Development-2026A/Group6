747a5e914adbee71660e1d6ed6517290fb5e9a42
Kochiya Shiina <s4117172@rmit.edu.vn>
2026-04-13 13:50:27 +0700

    adding prop so typescript could shut up

--- src/components/ui/PlayBox.tsx
+++ src/components/ui/PlayBox.tsx
@@ -1,8 +1,10 @@
 "use client"
 
 import React from "react"
-
-export default function PlayBox({char}) {
+type CharTypeProp = {
+    char: "X" | "O" | string
+}
+export default function PlayBox({char}: CharTypeProp) {
     return (
         <div className={char ? "bg-gray-500 w-20 h-20 text-center text-white flex items-center justify-center" : "bg-gray-200 w-20 h-20 text-center text-white flex items-center justify-center"}>
             <div className={'text-3xl'}>{char}</div>
