f230b0defcbe309ff6145c5df11b8ce60418e9a3
Kochiya Shiina <s4117172@rmit.edu.vn>
2026-04-18 10:19:12 +0700

    add another route in navbar

--- src/components/ui/NavBar.tsx
+++ src/components/ui/NavBar.tsx
@@ -14,7 +14,7 @@ export default function NavBar() {
 					<div className="hidden sm:flex sm:items-center space-x-4">
 						<a href="." className="px-3 py-2 rounded-md text-sm font-medium text-gray-700 hover:bg-gray-100">Home</a>
 						<a href="gamemodes" className="px-3 py-2 rounded-md text-sm font-medium text-gray-700 hover:bg-gray-100">Game Modes</a>
-						<a href="#" className="px-3 py-2 rounded-md text-sm font-medium text-gray-700 hover:bg-gray-100">Pricing</a>
+						<a href="subscription" className="px-3 py-2 rounded-md text-sm font-medium text-gray-700 hover:bg-gray-100">Pricing</a>
 					</div>
 
                     <a href={"#"}>
