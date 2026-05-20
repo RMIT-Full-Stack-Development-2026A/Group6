cb389e4b22610a319214f4d012bc391ea5ad1d00
Ricobaohoc <s4038330@rmit.edu.vn>
2026-05-20 23:13:47 +0700

    recover stash

--- src/app/privacy/layout.tsx
+++ src/app/privacy/layout.tsx
@@ -25,7 +25,7 @@ export default function RootLayout({
   return (
       <html lang="en">
       <link rel="preconnect" href="https://fonts.googleapis.com"/>
-      <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin/>
+      <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous"/>
       <link href="https://fonts.googleapis.com/css2?family=Courier+Prime:ital,wght@0,400;0,700;1,400;1,700&display=swap"
             rel="stylesheet"/>
       <body
