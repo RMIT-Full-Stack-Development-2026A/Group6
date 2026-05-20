84f72afdf84e1f3bbfcf1fb4b19d9291ba0ee8f0
Ricobaohoc <s4038330@rmit.edu.vn>
2026-05-21 00:23:35 +0700

    Revert "bug ?"
    This reverts commit 10fe63bea264fe173d6b18ab197631adf3e2e224.

--- src/app/privacy/layout.tsx
+++ src/app/privacy/layout.tsx
@@ -1,14 +1,38 @@
 import type { Metadata } from "next";
+import { Geist, Geist_Mono } from "next/font/google";
+import "../globals.css";
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
 
 export const metadata: Metadata = {
   title: "Privacy Policy",
   description: "Privacy Policy",
 };
 
-export default function PrivacyLayout({
+export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  return <>{children}</>;
+  return (
+      <html lang="en">
+      <link rel="preconnect" href="https://fonts.googleapis.com"/>
+      <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin/>
+      <link href="https://fonts.googleapis.com/css2?family=Courier+Prime:ital,wght@0,400;0,700;1,400;1,700&display=swap"
+            rel="stylesheet"/>
+      <body
+          className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+      >
+      {children}
+      </body>
+      </html>
+  );
 }
